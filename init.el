f046602b2a90ba30864243a37b04420d92b44130
bagrounds <bagrounds@gmail.com>
2017-09-01 17:42:25 -0700

    [MINOR][ADD] use-package

--- init.el
+++ init.el
@@ -4,6 +4,15 @@
 
 (package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+  (require 'diminish)
+  (require 'bind-key)
+
 (custom-set-variables
   '(custom-safe-themes
     '("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default))
