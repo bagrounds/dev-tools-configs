141c969c863ad51d0d50d6cfbe36733259ef004e
bagrounds <bagrounds@gmail.com>
2017-09-08 17:18:47 -0700

    [MINOR][ADD] xmonad.hs .xmobarrc and .stalonetrayrc

---
+++ xmonad.hs
@@ -0,0 +1,37 @@
+import XMonad
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Config.Desktop
+
+main :: IO ()
+main =
+  let
+    myBar = "xmobar"
+    myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "[" "]" }
+    toggleStrutsKey XConfig {} = (mod4Mask, xK_b)
+    myConfig = desktopConfig {
+      modMask = mod4Mask,
+      manageHook = manageDocks <+> manageHook desktopConfig,
+      layoutHook = avoidStruts $ layoutHook desktopConfig,
+      startupHook =
+        let startupPrograms = [
+              "~/bin/set-path",
+              "pgrep stalonetray || stalonetray",
+              "xscreensaver",
+              "xrdb -load ~/.Xdefaults",
+              "xrandr --output DP2-1 --auto --above eDP1",
+              "~/bin/remap-caps-to-ctrl",
+              "nm-applet"
+              ]
+        in sequence_ $ map spawn startupPrograms,
+      terminal = "xterm"
+    }
+    myKeys = [
+      ((mod4Mask, xK_i), spawn "google-chrome"),
+      ((mod4Mask, xK_Return), spawn "xterm"),
+      ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+      ]
+    action = statusBar myBar myPP toggleStrutsKey $ additionalKeys myConfig myKeys
+  in action >>= xmonad
+
