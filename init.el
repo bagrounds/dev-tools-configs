962cdbd88f406492fbb734c2b229481451fb77c7
bagrounds <bagrounds@gmail.com>
2018-03-02 08:40:35 -0800

    [MINOR][ADD] melpa-stable and gnu (elpa) package archives

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 (require 'package)
 
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 
 (setq-default vc-follow-symlinks t)
 
